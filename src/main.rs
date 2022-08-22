294a318c31407f364dfc30b940ec5eee0bcd6dbb
tah-dah <markcfranz13@gmail.com>
2022-08-22 14:33:22 -0500

    added user input

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use substring::Substring;
+use std::io::stdin;
 
 
 
 fn main() {
-    let string = "launchcode";
-    let  backstr = string.substring(3,10).to_owned();
-    let  new_string = string.substring(0,3).to_owned();
+    let mut string =String::new(); // user input string
+    stdin().read_line(&mut string)
+        .ok()
+        .expect("failed readline");
     
-    let fullstr = backstr + &new_string;
-     
-    println!("{}", fullstr);
-
+        if string.len() > 3 {
+            let _firstthree = string.substring(0, 3).to_owned();
+            //let substr = string.len() + _firstthree.len();
+            let backstr = string.substring(3,string.len()).to_owned();
+            //let newstr = backstr +&_firstthree; 
+                println!("{}", backstr +&_firstthree);
+        } else {
+                 println!("{}", "use a larger string");
+    }
 }
 
 
